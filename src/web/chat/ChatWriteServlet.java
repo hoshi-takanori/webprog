d04a576d1481c2e0addee3f30a39534a075a84a2
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2014-06-01 22:46:42 +0900

    implement ChatWriteServlet class

--- src/web/chat/ChatWriteServlet.java
+++ src/web/chat/ChatWriteServlet.java
@@ -1,5 +1,8 @@
 package web.chat;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import web.example.BasicView;
 import web.servlet.Request;
 import web.servlet.Response;
@@ -25,9 +28,57 @@ public class ChatWriteServlet implements Servlet {
 	 */
 	@Override
 	public void service(Request request, Response response) {
+		if (request.getMethod().equals(Request.METHOD_POST)) {
+			handlePost(request, response);
+		} else {
+			handleGet(request, response);
+		}
+	}
+
+	/**
+	 * GET リクエストを処理する。
+	 * @param request リクエスト (入力)
+	 * @param response レスポンス (出力)
+	 */
+	public void handleGet(Request request, Response response) {
 		BasicView view = new BasicView(response);
 		view.printHead(getName(), view.cssLinkTag("/style.css"));
-		response.println("<p>この中身を何とかする。</p>");
+
+		response.println("<p>なんか書いてね。</p>");
+		response.println("<form method=\"POST\" action=\"/write\">");
+		response.println("なまえ：<input type=\"text\" name=\"username\"><br>");
+		response.println("かきこみ：<input type=\"text\" name=\"message\"><br>");
+		response.println("<input type=\"submit\" value=\"かきこむ\">");
+		response.println("</form>");
+
 		view.printTail();
+	}
+
+	/**
+	 * POST リクエストを処理する。
+	 * @param request リクエスト (入力)
+	 * @param response レスポンス (出力)
+	 */
+	public void handlePost(Request request, Response response) {
+		String username = request.getDecodedParameter("username");
+		String message = request.getDecodedParameter("message");
+
+		String sql = "insert into chat_log (user_name, message) values (?, ?)";
+		try (DBCommon db = new DBCommon();
+				PreparedStatement st = db.getConnection().prepareStatement(sql)) {
+			st.setString(1, username);
+			st.setString(2, message);
+			int numRows = st.executeUpdate();
+			if (numRows == 1) {
+				BasicView view = new BasicView(response);
+				view.printHead(getName(), view.cssLinkTag("/style.css"));
+				response.println("<p>書き込みました。</p>");
+				view.printTail();
+			} else {
+				response.setError(Response.STATUS_ERROR, "書き込み失敗しました。");
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			response.setError(Response.STATUS_ERROR, e);
+		}
 	}
 }
